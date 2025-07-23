eb29c1f9dae8362e411094de8d8dfa54728d4307
Eric <148642826+EricNSuarez@users.noreply.github.com>
2025-07-23 14:02:51 -0300

    removed class from job_posting_datetime snippet as it was missing in some cases

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ def main():
     for job in page_jobs:
         base_card_div = job.find("div", {"class": "base-card"})
         job_posting_id = base_card_div.get("data-entity-urn").split(":")[3]
-        job_posting_datetime = job.find("time", class_="job-search-card__listdate").get("datetime", None)
+        job_posting_datetime = job.find("time").get("datetime", None)
         job_posting_ids.append({
             "id": job_posting_id,
             "datetime": job_posting_datetime,
