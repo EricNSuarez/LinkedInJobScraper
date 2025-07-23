76a7f79d9ad01e126c8806190547eaf7a9962e11
Eric <148642826+EricNSuarez@users.noreply.github.com>
2025-07-23 11:36:30 -0300

    fix job_posting_datetime snippet to correctly use get method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import bs4
 import requests
 from bs4 import BeautifulSoup
 import logging
@@ -83,7 +84,7 @@ def main():
     for job in page_jobs:
         base_card_div = job.find("div", {"class": "base-card"})
         job_posting_id = base_card_div.get("data-entity-urn").split(":")[3]
-        job_posting_datetime = job.find("time", {"class": "job-search-card__listdate"})["datetime"] or None
+        job_posting_datetime = job.find("time", {"class": "job-search-card__listdate"}).get("datetime") or None
         job_posting_ids.append({
             "id": job_posting_id,
             "datetime": job_posting_datetime,
